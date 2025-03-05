587c2e319d0616debb2f865407ec886149a4b78f
Will Snider <snideriw@miamioh.edu>
2025-03-05 14:24:34 -0500

    added skewness getter function

--- Galaxy.h
+++ Galaxy.h
@@ -51,6 +51,8 @@ public:
     getSize() const { return (int)population.size(); }
     PosMat&
     getCOM() { return centerOfMass; }
+    double
+    getSkewness() { return skewness; }
     bool
     isUniformTime() const;
     double
